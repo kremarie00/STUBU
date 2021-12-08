7ef6d68255748ce6b28f24dd43533afa974c49ad
kremarie00 <92197036+kremarie00@users.noreply.github.com>
2021-12-08 23:30:14 +0800

    first

--- Student_Buddy_Final_Project/stubu/lib/main.dart
+++ Student_Buddy_Final_Project/stubu/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'StudyBuddy',
       theme: ThemeData(
         // This is the theme of your application.
         //
